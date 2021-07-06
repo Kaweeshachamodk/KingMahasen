944dfebe79da0c0b2b46c40894a17f3e7fcbb173
RAVANA-SL <85247716+RAVANA-SL@users.noreply.github.com>
2021-07-06 21:13:18 +0530

    Update memes.js

--- plugins/memes.js
+++ plugins/memes.js
@@ -49,7 +49,7 @@ if (Config.WORKTYPE == 'private') {
             bottomText: bottomText,
         }, async function(err) {
             if(err) throw new Error(err)
-            await message.client.sendMessage(message.jid, fs.readFileSync('asena-meme.png'), MessageType.image, {filename: 'asena-meme.png', mimetype: Mimetype.png, caption: 'Made by WhatsAsena'});
+            await message.client.sendMessage(message.jid, fs.readFileSync('asena-meme.png'), MessageType.image, {filename: 'asena-meme.png', mimetype: Mimetype.png, caption: 'Made by 𝓚𝓲𝓷𝓰𝓜𝓪𝓱𝓪𝓼𝓮𝓷 v 1.0 _____ 🤞😎🔰'});
             await info.delete();    
         });
     }));
@@ -87,7 +87,7 @@ else if (Config.WORKTYPE == 'public') {
             bottomText: bottomText,
         }, async function(err) {
             if(err) throw new Error(err)
-            await message.client.sendMessage(message.jid, fs.readFileSync('asena-meme.png'), MessageType.image, {filename: 'asena-meme.png', mimetype: Mimetype.png, caption: 'Made by WhatsAsena'});
+            await message.client.sendMessage(message.jid, fs.readFileSync('asena-meme.png'), MessageType.image, {filename: 'asena-meme.png', mimetype: Mimetype.png, caption: 'Made by 𝓚𝓲𝓷𝓰𝓜𝓪𝓱𝓪𝓼𝓮𝓷 v 1.0 _____ 🤞😎🔰'});
             await info.delete();    
         });
     }));
